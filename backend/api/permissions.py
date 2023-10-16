ad1a093a0a2f5207aab91433ada3d5b3f723697e
volk7 <volk7201@mail.ru>
2023-10-16 22:56:18 +0500

    Первая версия приложения

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,29 +4,25 @@ from rest_framework import permissions
 class AdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_staff
-        )
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_staff
-        )
+                )
 
 
 class AuthorOrAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
-        )
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_staff
-        )
+                )
